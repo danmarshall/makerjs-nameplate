715168f7b78e2b2f5d3ecb6e6575d1447a8b5972
Dan Marshall <danmar@microsoft.com>
2016-09-21 22:35:37 -0700

    hello world

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ var opentype = require('opentype.js');
 //download to local from http://microsoft.github.io/maker.js/fonts/allertastencil/AllertaStencil-Regular.ttf
 var font = opentype.loadSync('AllertaStencil-Regular.ttf');
 
-var nameplate = new Nameplate(font, 'test', 100, 10, 10, 3, true);
+var nameplate = new Nameplate(font, 'hello world', 100, 20, 10, 3, true);
 
 console.log(makerjs.exporter.toSVG(nameplate));
